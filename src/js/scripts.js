17a2d8a048d8d6127328223baca72fa2c3b60a9d
andoryweb <andoryweb@gmail.com>
2020-06-08 07:46:11 +0300

    Modals popup

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,4 @@
+/* work-slider */
 $('.work-slider').slick({
   infinite: true,
   slidesToShow: 3,
@@ -20,4 +21,93 @@ $('.work-slider').slick({
       }
     }
   ]
+});
+
+/* Modals popups */
+/* modal-bathroom */
+$(document).ready(function() {
+  $('#openBathroom').on("click", function() {
+    $('#modal-bathroom').show();
+  });
+  $('#close-bathroom').on("click", function() {
+    $('#modal-bathroom').hide();
+  });
+});
+/* modal-flat */
+$(document).ready(function() {
+  $('#openFlat').on("click", function() {
+    $('#modal-flat').show();
+  });
+  $('#close-flat').on("click", function() {
+    $('#modal-flat').hide();
+  });
+});
+/* modal-offices */
+$(document).ready(function() {
+  $('#openOffices').on("click", function() {
+    $('#modal-offices').show();
+  });
+  $('#close-offices').on("click", function() {
+    $('#modal-offices').hide();
+  });
+});
+/* modal-building */
+$(document).ready(function() {
+  $('#openBuilding').on("click", function() {
+    $('#modal-building').show();
+  });
+  $('#close-building').on("click", function() {
+    $('#modal-building').hide();
+  });
+});
+/* modal-kitchen */
+$(document).ready(function() {
+  $('#openKitchen').on("click", function() {
+    $('#modal-kitchen').show();
+  });
+  $('#close-kitchen').on("click", function() {
+    $('#modal-kitchen').hide();
+  });
+});
+/* modal-mansion */
+$(document).ready(function() {
+  $('#openMansion').on("click", function() {
+    $('#modal-mansion').show();
+  });
+  $('#close-mansion').on("click", function() {
+    $('#modal-mansion').hide();
+  });
+});
+
+/* offices-slider */
+$('.offices-slider').slick({
+  autoplay: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: $('.offices-prev'),
+  nextArrow: $('.offices-next')
+});
+/* building-slider */
+$('.building-slider').slick({
+  autoplay: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: $('.building-prev'),
+  nextArrow: $('.building-next')
+});
+/* kitchen-slider */
+$('.kitchen-slider').slick({
+  autoplay: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: $('.kitchen-prev'),
+  nextArrow: $('.kitchen-next')
+});
+/* mansion-slider */
+$('.mansion-slider').slick({
+  autoplay: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: $('.mansion-prev'),
+  nextArrow: $('.mansion-next')
 });
